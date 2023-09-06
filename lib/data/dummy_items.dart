31535f9cefca73c9d73807dd77c8a6074fda64d9
Trevor Toni <badrobot.f4@gmail.com>
2023-09-06 14:59:15 +0300

    Added dummy data

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,24 @@
+import 'package:shopping_list/models/grocery_item_model.dart';
+import 'package:shopping_list/data/categories_data.dart';
+import 'package:shopping_list/models/category_model.dart';
+
+final groceryItems = [
+  GroceryItem(
+    id: 'a',
+    name: 'Milk',
+    quantity: 1,
+    category: categories[Categories.dairy]!,
+  ),
+  GroceryItem(
+    id: 'b',
+    name: 'Bananas',
+    quantity: 5,
+    category: categories[Categories.fruit]!,
+  ),
+  GroceryItem(
+    id: 'c',
+    name: 'Beef Steak',
+    quantity: 1,
+    category: categories[Categories.meat]!,
+  ),
+];
